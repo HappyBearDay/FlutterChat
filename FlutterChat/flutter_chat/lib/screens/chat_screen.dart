9123e5ff8bc67a553d6213f39364a658e3449b14
happybearday <anishamroun2@gmail.com>
2020-11-06 23:52:15 +0100

    The end

---
+++ FlutterChat/flutter_chat/lib/screens/chat_screen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/models/message_model.dart';
+import 'package:flutter_chat/models/user_model.dart';
+
+class ChatScreen extends StatefulWidget {
+
+  final User user;
+  ChatScreen({this.user});
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe){
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 8.0,
+            bottom: 8.0,
+            left: isMe? 80.0 : 0,
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+          decoration: BoxDecoration(
+              color: isMe? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+              borderRadius: isMe? BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomLeft: Radius.circular(15.0),
+              ) : BorderRadius.only(
+                topRight: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0),
+              )
+          ),
+          width: MediaQuery.of(context).size.width * 0.75,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(message.time, style: TextStyle(color: Colors.blueGrey, fontSize: 16.0, fontWeight: FontWeight.w600),),
+              SizedBox(height: 8.0),
+              Text(message.text, style: TextStyle(color: Colors.blueGrey, fontSize: 16.0, fontWeight: FontWeight.w600))
+            ],
+          ),
+          ),
+        isMe? SizedBox.shrink(): IconButton(
+          icon: message.isLiked ?Icon(Icons.favorite) : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked ? Colors.red : Colors.black,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildMessageComposer(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.photo),
+              color: Theme.of(context).primaryColor,
+              onPressed: ()=>{}),
+          Expanded(
+              child: TextField(
+                textCapitalization: TextCapitalization.sentences,
+                onChanged: (value){
+                  setState(() {
+                    
+                  });
+                },
+                decoration: InputDecoration(hintText: "Send a message..."),
+              )
+          ),
+          IconButton(
+              icon: Icon(Icons.send),
+              color: Theme.of(context).primaryColor,
+              onPressed: ()=>{}),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.user.name,
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              )
+          ),
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.more_horiz),
+              iconSize: 30.0,
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          ]
+      ),
+      body: GestureDetector(
+        onTap: ()=>FocusScope.of(context).unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  )
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                      itemCount: messages.length,
+                      itemBuilder: (BuildContext context, int index){
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id  ;
+                    return _buildMessage(message, isMe);
+                  }),
+                ),
+              ),
+            ),
+            _buildMessageComposer()
+          ],
+        ),
+      )
+    );
+  }
+}
